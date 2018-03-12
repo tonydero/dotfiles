9befc7c136c510fffb2e56c20a6feb9cd6d2cb35
Tony DeRocchis <tonyderocchis@gmail.com>
2018-03-11 23:15:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,6 +50,8 @@ then
   sudo add-apt-repository ppa:aguignard/ppa
   sudo apt-get update
   sudo apt-get install libxcb-xrm-dev
+  
+  sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 fi
 
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
